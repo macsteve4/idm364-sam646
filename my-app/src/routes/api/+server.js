30510e2c428f73ac8b5bc96c1fd329b350a21aa2
macsteve4 <71797763+macsteve4@users.noreply.github.com>
2024-03-22 02:42:26 -0400

    API Update
    - changed API Response to format more readable,  à la pretty-print

--- my-app/src/routes/api/+server.js
+++ my-app/src/routes/api/+server.js
@@ -6,8 +6,17 @@ export async function GET() {
         .from('noodles')
         .select('*');
 
-        return new Response(JSON.stringify({noodles: data}), {
-            headers: { 'Content-Type': 'application/json'},
+        if (error) {
+            console.error('Error fetching noodles:', error);
+            return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+                headers: { 'Content-Type': 'application/json' },
+            });
+        }
+
+        const prettyData = JSON.stringify({ noodles: data }, null, 2);
+
+        return new Response(prettyData, {
+            headers: { 'Content-Type': 'application/json' },
         });
     } catch (error) {
         console.error('Error fetching ramen noodles:', error);
